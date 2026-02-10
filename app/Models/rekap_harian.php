f9c4216a2c9ab4ea1d326d0a753f9275fe82d149
egisaputra1784 <eginovianisaputra313@gmail.com>
2026-02-10 10:59:14 +0700

    model baru

---
+++ app/Models/rekap_harian.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class rekap_harian extends Model
+{
+    protected $table = 'rekap_harian';
+    public $timestamps = false;
+    protected $guarded = [];
+}
